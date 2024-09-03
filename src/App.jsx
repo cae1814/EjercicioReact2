ffd980e4763a36178285809b0aa1ea04cf37f423
Antonio Banegas <cae1814@gmail.com>
2024-09-03 09:39:31 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { EjercicioReactJs } from './ComponentScreen/EjercicioReactJs';
+import { EjercicioReactJs2 } from './ComponentScreen/EjercicioReactJs2';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+export const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<EjercicioReactJs/>}  />
+          <Route path='/ejercicioReactJS2' element={<EjercicioReactJs2/>}  />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
